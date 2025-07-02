7e21c380d92191079cf08cd7d428b266c0cc3df0
minh.mai <minh.mai@sortkeeter.com>
2025-07-02 20:54:15 +0700

    init code

---
+++ src/test/java/com/example/admission/admissionswebsite/AdmissionsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admission.admissionswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionsWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
